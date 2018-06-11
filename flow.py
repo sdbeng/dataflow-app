b42cea9b3cca652f0b5c46db6935bacadf2ad356
Sergio Barrera <sdbeng@gmail.com>
2018-06-11 09:14:44 -0700

    Fix indentation

--- flow.py
+++ flow.py
@@ -3,11 +3,12 @@ def clinic():
     print("Do you take the door on the left or the right?")
     answer = input("Type left or right and hit 'Enter'.").lower()
     if answer == "left" or answer == "l":
-      print("This is the Office, you hear parrots!")
+        print("This is the Office, you hear parrots!")
     elif answer == "right" or answer == "r":
-      print("Of course this is the Work Room, I've told you that already!")
+        print("Of course this is the Work Room, I've told you that already!")
     else:
-      print("You didn't pick left or right! Try again.")
-      clinic()
+        print("You didn't pick left or right! Try again.")
+        clinic()
+
 
 clinic()
